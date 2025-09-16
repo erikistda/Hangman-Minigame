e58ddb725015cc6b296e8435aedd5ba969a8e62e
benjamin kähr <benjkaehr1@gmail.com>
2025-09-16 09:10:22 +0200

    Window Setup 2

--- Hangman Minigame/Hangman_Minigame.py
+++ Hangman Minigame/Hangman_Minigame.py
@@ -1,1 +1,12 @@
-print("Willkommen zum Hangman Minigame!")
+import tkinter as tk
+
+# Window setup
+def on_escape(event=None):
+    root.destroy()
+root = tk.Tk()
+root.title("Hangman Minigame")
+root.attributes("-fullscreen",True)
+root.configure(bg="beige")
+root.bind_all("<Escape>",on_escape)
+root.resizable(False,False)
+root.mainloop()

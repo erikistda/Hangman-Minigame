8856682fe436df15017a8d7d2f3deeb41f0b6688
erikistda <benjkaehr1@gmail.com>
2025-09-17 08:08:53 +0200

    Code von Di 16.09.2025
    Added a Button on lines 13-26 and made it clickable to say "You Clicked me". Also the Background is now a light blue and no longer beige

--- Hangman Minigame/Hangman_Minigame.py
+++ Hangman Minigame/Hangman_Minigame.py
@@ -6,7 +6,27 @@ def on_escape(event=None):
 root = tk.Tk()
 root.title("Hangman Minigame")
 root.attributes("-fullscreen",True)
-root.configure(bg="beige")
+root.configure(bg="#869fb3")
 root.bind_all("<Escape>",on_escape)
 root.resizable(False,False)
+
+#Button erstellen
+
+def buttonpress():
+    label = tk.Label(root, text="Du hast mich gedrückt!", font=("arial", 16))
+    label.place(x=680, y=10)
+
+button = tk.Button(
+    root,
+    text="Klick mich!",
+    font=("arial", 20),
+    bg="#869fb3",
+    command=buttonpress
+)
+button.place(x=700, y=450)
+
+
+#Screens
+
+
 root.mainloop()
